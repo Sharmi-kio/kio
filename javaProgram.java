51b4eeb41170102e1cec53d5286b4b49e06d9086
Sharmi-kio <masssharmi777@gmail.com>
2024-02-25 06:27:05 +0530

    kio

--- javaProgram.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.io.*;
-public class javaProgram{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.println("number"+n);
-    }
-}
